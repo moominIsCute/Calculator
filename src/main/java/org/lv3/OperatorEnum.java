661b96b1fc23299a7ac4058457e606b7c758ca29
moominIsCute <1happymoomin1@gmail.com>
2025-07-14 05:58:02 +0900

    레벨3 이넘

--- src/main/java/org/lv3/OperatorEnum.java
+++ src/main/java/org/lv3/OperatorEnum.java
@@ -1,5 +1,15 @@
 package org.lv3;
 
 public enum OperatorEnum {
-    Add('+')
+    ADD('+'), SUBTRACT('-'), MULTIPLY('*'), DIVIDE('/');
+
+    final char operatorChar;
+
+    OperatorEnum(char operatorChar) {
+        this.operatorChar = operatorChar;
+    }
+
+    char getOperatorChar() {
+        return operatorChar;
+    }
 }
